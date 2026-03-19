bfa040efb3fd45b2b4679e256fa74e8b6a7a0ff7
Or Avni <tsukasasatodesu@gmail.com>
2026-03-20 01:10:45 +0200

    Add files via upload

---
+++ src/firebaseInit.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+
+  // Your firebase config block here
+
+};
+
+
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
